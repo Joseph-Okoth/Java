6b3f6daf1798ec1cd174a1d33702f42f6eba8167
Joseph Onyach <josephokothonyach@gmail.com>
2025-06-15 22:41:06 +0300

    Looping is awesome

---
+++ Loop_Control/For_Loop/Test.java
@@ -0,0 +1,11 @@
+package Loop_Control.For_Loop;
+
+public class Test {
+    public static void main(String args[]){
+        
+        for(int x = 10; x < 20; x = x+1){
+            System.out.print("Value of x : " + x);
+            System.out.print("\n");
+        }
+    }
+}
---
+++ Loop_Control/While_Loop/Test.java
@@ -0,0 +1,13 @@
+package Loop_Control.While_Loop;
+
+public class Test {
+    public static void main(String args[]){
+        int x = 10;
+
+        while( x < 20 ){
+            System.out.print("Value of x : " + x );
+            x++;
+            System.out.println("\n");
+        }
+    }
+}
