8bdf33b509a5b708d2c329368bb8b7ff618d1436
Joseph Onyach <josephokothonyach@gmail.com>
2025-06-15 23:15:01 +0300

    Enhanced loops

---
+++ Loop_Control/Do_While_Loop/Test.java
@@ -0,0 +1,13 @@
+package Loop_Control.Do_While_Loop;
+
+public class Test {
+    public static void main(String args[]){
+        int x = 10;
+
+        do{
+            System.out.print("Value of x : " + x );
+            x++;
+            System.out.print("\n");
+        }while( x < 20 );
+    }
+}
